2dd8483a0bc119937e815244a96c2d80a7f980e1
jenniferdevega <devega.jennifer.alvarez@gmail.com>
2023-11-06 17:13:04 +0800

    Added layout component for Nav bar

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,36 @@
+import { Outlet, Link } from "react-router-dom";
+function Layout(){
+
+    return(
+        <main className="d-flex flex-column min-vh-100">
+            <nav className="navbar navbar-expand-lg bg-black shadow">
+                <div className="container-fluid">
+                    <Link className="navbar-brand fw-bold text-light" to="/">React Routing Demo</Link>
+                    <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav text-light">
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to="about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-light" to="contact">Contact</Link>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="container-fluid p-3">
+                <Outlet></Outlet>
+            </div>
+
+            <footer className="bg-dark p-1 text-center text-white footer mt-auto">
+                <p>This is the footer</p>
+            </footer>
+        </main>
+    )
+}
+
+export default Layout;
